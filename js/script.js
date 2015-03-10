a7c8bc92d436aad79c691a202032661b3e522aae
Andela Dev <andela@OLAJIDEs-MacBook-Pro-2.local>
2015-03-10 21:45:00 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -18,21 +18,26 @@ var soundSearch = {
       
       $.getJSON(this.url+"/tracks.json?q="+trackName+"?&amp;client_id="+this.id, {limit: 30}, function (response) {
         console.log(response);
+        
       
         var view = '<h2>' + 'MATCHES:' + '</h2>';
         $.each(response, function (index, track) {
-          view += "<h6>"+"Title : "+track.title +"</h6>";
+          var play = track.stream_url+ "?client_id=" + soundSearch.id;
           if (typeof(track.artwork_url) != 'string') {
             view += "<img src="+'img/not_found.jpg'+">";
           }
           else {
-            view += "<img src="+track.artwork_url+">";
+            view += "<img class='img-thumbnail img-responsive' style='width:250px;height:150px' src="+track.artwork_url+">";
           }
-          view += '<a href="'+track.permalink_url+'" target="_blank">'+ "Play" + '</a>';
+          view += "<h6>"+"Title : "+track.title +"</h6>";
           view +="<h5>"+"Genre : " +track.genre+"<h5>";
+          view +="<audio controls><source src="+play+" type=audio/mpeg></audio>";
         })
+        //u should be adding(appending) this next line to your div that has class "row", and remove the elements inside that row
+        // "<div class='col-md-4'><div class=result><img class='img-thumbnail img-responsive' style='width:250px;height:150px' src="+track.artwork_url+"><br><h6>"+track.title+"</h6><br><h5>"+track.genre+"</h5><br><audio controls><br><source src="+play+" type=audio/mpeg></audio></div></div"
         $(".result").html(view);
       });
+
     }
 
 };
