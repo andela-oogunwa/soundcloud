f1761b43046ae7c84ab88d613f974cc008f89b0e
andela-oogunwa <fadekemi.ogunwa@andela.co>
2015-03-11 04:03:47 +0100

    worked on

--- js/script.js
+++ js/script.js
@@ -20,26 +20,22 @@ var soundSearch = {
         console.log(response);
         
       
-        var view = '<h2>' + 'MATCHES:' + '</h2>';
+        var view = '<span>';
         $.each(response, function (index, track) {
+           view += '<div class="col-md-4">';
           var play = track.stream_url+ "?client_id=" + soundSearch.id;
           if (typeof(track.artwork_url) != 'string') {
-            view += "<img src="+'img/not_found.jpg'+">";
+            view += "<img class='img-thumbnail'  src='img/not_found.jpg'>";
           }
           else {
-            view += "<img class='img-thumbnail img-responsive' style='width:250px;height:150px' src="+track.artwork_url+">";
+            view += "<img class='img-thumbnail img-responsive' src="+track.artwork_url+">";
           }
-          view += "<h6>"+"Title : "+track.title +"</h6>";
-          view +="<h5>"+"Genre : " +track.genre+"<h5>";
-          view +="<audio controls><source src="+play+" type=audio/mpeg></audio>";
+          view += "<h6>"+"Title : "+track.title.substr(0, 26).toLowerCase() + "...</h6>" +
+          "<h5>"+"Genre : " +track.genre+"</h5>" +
+          "<audio controls><source src="+play+" type=audio/mpeg></audio></div></span>";
         })
-        //u should be adding(appending) this next line to your div that has class "row", and remove the elements inside that row
-        // "<div class='col-md-4'><div class=result><img class='img-thumbnail img-responsive' style='width:250px;height:150px' src="+track.artwork_url+"><br><h6>"+track.title+"</h6><br><h5>"+track.genre+"</h5><br><audio controls><br><source src="+play+" type=audio/mpeg></audio></div></div"
-        $(".result").html(view);
       });
-
     }
-
 };
  $(document).ready(function() {
    soundSearch.first();
